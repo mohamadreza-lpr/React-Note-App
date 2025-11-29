7366e6990537c91f6ab09183e7913bd24e4a3775
m.pour <mlaalpoor@yahoo.com>
2025-11-29 22:08:45 +0330

    feat: made some changes

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -8,41 +8,37 @@ function NoteList({ noteLists, onRemoveNote, onComplete }) {
     };
     return (
         <>
-            {noteLists.length > 0 ? (
-                noteLists.map((n) => {
-                    return (
-                        <div key={n.id} className="note-item">
-                            <div className="note-item__header">
-                                <div className={n.completed ? "note-item completed" : ""}>
-                                    <h3 className={n.completed ? "title" : ""}> {n.title}</h3>
-                                    <p className="desc">{n.description}</p>
-                                </div>
-                                <div className="actions">
-                                    <button
-                                        className="trash"
-                                        onClick={() => {
-                                            onRemoveNote(n);
-                                        }}
-                                    >
-                                        ❌
-                                    </button>
-                                    <input
-                                        type="checkbox"
-                                        name="subscribe"
-                                        value={n.completed}
-                                        onChange={() => {
-                                            onComplete(n);
-                                        }}
-                                    />
-                                </div>
+            {noteLists.map((n) => {
+                return (
+                    <div key={n.id} className="note-item">
+                        <div className="note-item__header">
+                            <div className={n.completed ? "note-item completed" : ""}>
+                                <h3 className={n.completed ? "title" : ""}> {n.title}</h3>
+                                <p className="desc">{n.description}</p>
                             </div>
-                            <div className="note-item__footer">{new Date(n.created_at).toLocaleDateString("en-US", options)}</div>
+                            <div className="actions">
+                                <button
+                                    className="trash"
+                                    onClick={() => {
+                                        onRemoveNote(n);
+                                    }}
+                                >
+                                    ❌
+                                </button>
+                                <input
+                                    type="checkbox"
+                                    name="subscribe"
+                                    value={n.completed}
+                                    onChange={() => {
+                                        onComplete(n);
+                                    }}
+                                />
+                            </div>
                         </div>
-                    );
-                })
-            ) : (
-                <div>No note available</div>
-            )}
+                        <div className="note-item__footer">{new Date(n.created_at).toLocaleDateString("en-US", options)}</div>
+                    </div>
+                );
+            })}
         </>
     );
 }
--- src/components/NoteStatus.jsx
+++ src/components/NoteStatus.jsx
@@ -4,6 +4,9 @@ function NoteStatus({ notes }) {
     const allNotes = notes.length;
     const completedNotes = notes.filter((n) => n.completed).length;
     const openNotes = allNotes - completedNotes;
+
+    if(!allNotes) return <p>No Notes has already been added.</p>
+    
     return (
         <ul className="note-status">
             <li>
